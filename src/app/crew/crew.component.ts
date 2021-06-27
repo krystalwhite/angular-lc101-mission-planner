1f4246b6e3ddb25d53520cf17a33a2ee32b8fcf7
krystalwhite <krystalsnowhite@gmail.com>
2021-06-27 18:12:14 -0500

    complete studio, save for bonus

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,6 +13,8 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
@@ -26,4 +28,14 @@ export class CrewComponent implements OnInit {
     let index = this.crew.indexOf(member);
     this.crew.splice(index, 1);
   }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+ 
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
+
 }
