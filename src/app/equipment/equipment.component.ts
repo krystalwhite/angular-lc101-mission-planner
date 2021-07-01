f6fb9f33d08a8fef69511203303596fb216067a5
krystalwhite <krystalsnowhite@gmail.com>
2021-07-01 08:06:06 -0500

    can add and remove equipment and experiments; changed color of text

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ["Drones", "Food containers", "Oxygen tanks", "Habitat dome"];
+  equipmentBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addEquipment(item : string) {
+    this.equipment.push(item);
+  }
+
+  removeEquipment(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experiments : string[] = ["Mars soil sample", "Plant growth in habitat", "Human bone density"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addExperiment(item : string) {
+    this.experiments.push(item);
+  }
+
+  removeExperiment(item: string) {
+    let index = this.experiments.indexOf(item);
+    this.experiments.splice(index, 1);
+  }
 }
